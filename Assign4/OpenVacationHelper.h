9c496b74f4f1ee7c675c4e4ca75d0c8a100bf2c9
athony <athony@athonys-macbook.home>
2013-06-17 14:14:03 -0400

    starting assignment 6

---
+++ Assign4/OpenVacationHelper.h
@@ -0,0 +1,13 @@
+//
+//  OpenVacationHelper.h
+//  Assign4
+//
+//  Created by Anthony Zavadil on 6/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenVacationHelper : NSObject
+
+@end
---
+++ Assign4/OpenVacationHelper.m
@@ -0,0 +1,13 @@
+//
+//  OpenVacationHelper.m
+//  Assign4
+//
+//  Created by Anthony Zavadil on 6/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "OpenVacationHelper.h"
+
+@implementation OpenVacationHelper
+
+@end
